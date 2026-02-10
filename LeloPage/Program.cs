fa5413051466785256f756490e82a411b3dcb39e
J_L <jlwork0505@gmail.com>
2026-02-10 15:33:47 +0100

    claudeTouch

--- LeloPage/Program.cs
+++ LeloPage/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using LeloPage;
+using LeloPage.Services;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +10,20 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+// Add localization services
+builder.Services.AddLocalization();
+
+// Add culture service
+builder.Services.AddScoped<CultureService>();
+
+var host = builder.Build();
+
+// Initialize culture
+var cultureService = host.Services.GetRequiredService<CultureService>();
+await cultureService.InitializeAsync();
+
+Console.WriteLine($"Current Culture: {CultureInfo.CurrentCulture.Name}");
+Console.WriteLine($"Current UI Culture: {CultureInfo.CurrentUICulture.Name}");
+
+await host.RunAsync();
+
---
+++ LeloPage/Resources/SharedResources.cs
@@ -0,0 +1,10 @@
+namespace LeloPage.Resources
+{
+    /// <summary>
+    /// This class is intentionally empty.
+    /// It serves as a marker for the resource files.
+    /// </summary>
+    public class SharedResources
+    {
+    }
+}
